94a01b1e5f874ae3690e7b2d66f183e244185067
Barath <95070448+GodOfanjal@users.noreply.github.com>
2022-06-12 18:53:02 +0530

    Update start.py

--- TgProtectronBot/plugins/start.py
+++ TgProtectronBot/plugins/start.py
@@ -14,7 +14,7 @@ async def start(event):
     if event.is_private:
        await event.reply(PM_START_TEXT.format(event.sender.first_name), buttons=[
         [Button.inline("Help & Commands", data="help")],
-        [Button.url("Add Me To Your Group", "https://t.me/{BOT_USERNAME}?startgroup=true")]])
+        [Button.url("Add Me To Your Group", "https://t.me/TgProtectronBot?startgroup=true")]])
        return
 
     if event.is_group:
