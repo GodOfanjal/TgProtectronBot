5945a71c3e0daa77359444264b5c113f05385858
Barath <95070448+GodOfanjal@users.noreply.github.com>
2022-06-12 13:22:31 +0530

    Create utils.py

---
+++ TgProtectronBot/utils.py
@@ -0,0 +1,14 @@
+import sys
+import logging
+import importlib
+from pathlib import Path
+
+def load_plugins(plugin_name):
+    path = Path(f"TgProtectronBot/plugins/{plugin_name}.py")
+    name = "TgProtectronBot.plugins.{}".format(plugin_name)
+    spec = importlib.util.spec_from_file_location(name, path)
+    load = importlib.util.module_from_spec(spec)
+    load.logger = logging.getLogger(plugin_name)
+    spec.loader.exec_module(load)
+    sys.modules["NoinoiRobot.plugins." + plugin_name] = load
+    print(">> 𝙿𝙻𝚄𝙶𝙸𝙽 𝙰𝙳𝙳𝙴𝙳 𝚂𝚄𝙲𝙴𝚂𝚂𝙵𝚄𝙻𝙻𝚈 . . " + plugin_name)
