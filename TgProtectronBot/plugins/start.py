d5fa5a1af708894aef3505c0412a6b510fbcad9d
Barath <95070448+GodOfanjal@users.noreply.github.com>
2022-06-12 14:28:04 +0530

    Create start.py

---
+++ TgProtectronBot/plugins/start.py
@@ -0,0 +1,22 @@
+from TgProtectronBot import Stark
+from telethon import events, Button
+
+PM_START_TEXT = """
+**Hi {}**
+I am simple manager robot whos help to you for manage your group
+
+**Click the below help button for the open help menu!**
+"""
+
+@Stark.on(events.NewMessage(pattern="^[?!/]start$"))
+async def start(event):
+
+    if event.is_private:
+       await event.reply(PM_START_TEXT.format(event.sender.first_name), buttons=[
+        [Button.inline("Help & Commands", data="help")],
+        [Button.url("Developer", "https://t.me/itzmecrazy")]])
+       return
+
+    if event.is_group:
+       await event.reply("**I AM ALIVE ✨**")
+       return
